e8ddcf5a2dbe73f2545cca4d5400a539bd8f5e1a
PoeTien <43399875+PoeTien@users.noreply.github.com>
2021-06-14 00:04:17 +0800

    Test JS auto change href

---
+++ js/userAgent.js
@@ -0,0 +1,13 @@
+
+<script type="text/javascript">
+
+
+var URL = document.location.toString();
+var useragent = navigator.userAgent;
+useragent = useragent.toLowerCase();
+
+if( useragent.indexOf('iphone') != -1 ) document.getElementById("myLink").href="sms:+886953789076&body=嗨!Poe,可以收到我的簡訊嗎?"; //iphone
+else if( useragent.indexOf('ipad') != -1 || useragent.indexOf('ipod') != -1) document.getElementById("myLink").href="sms:+886953789076&body=嗨!Poe,可以收到我的簡訊嗎?"; //ipad
+else if( useragent.indexOf('android') != -1 ) document.getElementById("myLink").href="sms:+886953789076?body=嗨!Poe,可以收到我的簡訊嗎?"; 
+
+</script>
