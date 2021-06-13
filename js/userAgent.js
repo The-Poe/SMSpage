2d525266526cb533cd3d01731788e6dfeaed5170
PoeTien <43399875+PoeTien@users.noreply.github.com>
2021-06-14 00:09:13 +0800

    Add init func

--- js/userAgent.js
+++ js/userAgent.js
@@ -2,12 +2,18 @@
 <script type="text/javascript">
 
 
+  $(document).ready(init);
+    function init(){
+
 var URL = document.location.toString();
 var useragent = navigator.userAgent;
 useragent = useragent.toLowerCase();
 
-if( useragent.indexOf('iphone') != -1 ) document.getElementById("myLink").href="sms:+886953789076&body=嗨!Poe,可以收到我的簡訊嗎?"; //iphone
-else if( useragent.indexOf('ipad') != -1 || useragent.indexOf('ipod') != -1) document.getElementById("myLink").href="sms:+886953789076&body=嗨!Poe,可以收到我的簡訊嗎?"; //ipad
-else if( useragent.indexOf('android') != -1 ) document.getElementById("myLink").href="sms:+886953789076?body=嗨!Poe,可以收到我的簡訊嗎?"; 
+     if( useragent.indexOf('iphone') != -1 ) document.getElementById("myLink").href="sms:+886953789076&body=嗨!Poe,可以收到我的簡訊嗎?"; //iphone
+     else if( useragent.indexOf('ipad') != -1 || useragent.indexOf('ipod') != -1) document.getElementById("myLink").href="sms:+886953789076&body=嗨!Poe,可以收到我的簡訊嗎?"; //ipad
+     else if( useragent.indexOf('android') != -1 ) document.getElementById("myLink").href="sms:+886953789076?body=嗨!Poe,可以收到我的簡訊嗎?"; 
+     
+    }
+
 
 </script>
